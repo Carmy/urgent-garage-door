d5e6e6bf93b6551ad3cf9499d767780f3749d982
Carmy <carmy25@gmail.com>
2025-05-13 23:54:20 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+function toggleTheme() {
+  document.body.classList.toggle('dark-mode');
+  localStorage.setItem('theme', document.body.classList.contains('dark-mode') ? 'dark' : 'light');
+}
+
+window.addEventListener('DOMContentLoaded', () => {
+  const savedTheme = localStorage.getItem('theme');
+  if (savedTheme === 'dark') {
+    document.body.classList.add('dark-mode');
+  }
+});
+
+function toggleMobileMenu() {
+    document.querySelector('.main-nav').classList.toggle('open');
+  }
+  
